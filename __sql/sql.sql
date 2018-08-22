a5a78db832802b18a2449f7d221d8ee2e37d7559
Felipe <felipe.ferreira0192@gmail.com>
2018-08-22 17:01:39 -0300

    Ajuste no arquivo .sql

--- __sql/sql.sql
+++ __sql/sql.sql
@@ -1,9 +1,7 @@
 /*Criar a base de dados*/
-
-create database crud_php;
+create database crud;
 
 /*Criação da tabela filme*/
-
 create table filme(
     id int(5) not null primary key AUTO_INCREMENT,
     tituloOr varchar(50) not null,
@@ -12,12 +10,3 @@ create table filme(
     diretor varchar(50),
     genero varchar(50)
 );
-
-/*Apagar todos os dados da tabela*/
-
-truncate filme;
-
-/* Excluir registro*/
-
-DELETE FROM filme WHERE filme.id = 2;
-
